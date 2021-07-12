e8c946ba6772670dd48f4cf79d1df99b146a0ea7
Gianine <gianine1508@gmail.com>
2021-07-12 11:25:26 -0300

    criação da tabela endereco

--- app/configs/database.py
+++ app/configs/database.py
@@ -6,3 +6,5 @@ db = SQLAlchemy()
 def init_app(app: Flask):
         db.init_app(app)
         app.db = db
+
+        from app.models.endereco_model import Endereco
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.configs.database import db
+
+from .endereco_model import Endereco
