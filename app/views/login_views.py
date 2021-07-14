7723bf2a6dae8bdbe78e8325e81452886c62e8eb
andersonvaler <andersonvaler@gmail.com>
2021-07-14 12:06:54 -0300

    fix: variables name

--- app/views/login_views.py
+++ app/views/login_views.py
@@ -3,19 +3,19 @@ from http import HTTPStatus
 
 from werkzeug.utils import redirect
 from app.models.clientes_model import Clientes
-from app.models.lojistas import Logistas
+from app.models.lojistas_model import Lojistas
 
 def create_user():
 
     email_user = request.get_json("email")
     cliente = Clientes.query.filter_by(email=email_user).first()
-    lojista = Logistas.query.filter_by(email=email_user).first()
+    lojista = Lojistas.query.filter_by(email=email_user).first()
     msg={"Usuário não encontrado"}
     if cliente:
         user = cliente
     elif lojista:
         user = lojista
-    if user and user.check_password(password_user):
+    if user and user.check_password(user.password):
         return redirect("/"), HTTPStatus.OK
     return msg, HTTPStatus.NOT_FOUND
     
