1f7d8882f54f357fec6b8c952a3bd70a4cfa75de
andersonvaler <andersonvaler@gmail.com>
2021-07-15 16:50:43 -0300

    feat: add message on default route

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -8,10 +8,12 @@ def init_app(app: Flask):
     from .carrinho_views import bp as bp_carrinho 
     from .produtos_views import bp as bp_product
     from .gerar_vendas_views import bp as bp_gerar_vendas
+    from .home_view import bp as bp_home
     app.register_blueprint(bp_product)
     app.register_blueprint(bp_signup)
     app.register_blueprint(bp_login)
     app.register_blueprint(bp_perfil)
     app.register_blueprint(bp_carrinho)
     app.register_blueprint(bp_gerar_vendas)
+    app.register_blueprint(bp_home)
 
