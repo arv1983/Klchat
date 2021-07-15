e7e43e0a91df244b25bbe94cf1bfe15aaef39b00
Gianine <gianine1508@gmail.com>
2021-07-15 11:24:49 -0300

    end point gerar venda

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -7,9 +7,11 @@ def init_app(app: Flask):
     from .login_views import bp as bp_login
     from .carrinho_views import bp as bp_carrinho 
     from .produtos_views import bp as bp_product
+    from .gerar_vendas_views import bp as bp_gerar_vendas
     app.register_blueprint(bp_product)
     app.register_blueprint(bp_signup)
     app.register_blueprint(bp_login)
     app.register_blueprint(bp_perfil)
     app.register_blueprint(bp_carrinho)
+    app.register_blueprint(bp_gerar_vendas)
 
