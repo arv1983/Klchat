6693a0382e3c4f761cf5ade76c17e2d91e671b44
andersonvaler <andersonvaler@gmail.com>
2021-07-14 23:39:58 -0300

    feat: edit models

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,8 +4,10 @@ from flask import Flask
 def init_app(app: Flask):
     from .signup_views import bp as bp_signup
     from .perfil_cliente import bp as bp_perfil
-    from .login_views import bp as bp_login    
+    from .login_views import bp as bp_login
+    from .carrinho_views import bp as bp_carrinho    
     app.register_blueprint(bp_signup)
     app.register_blueprint(bp_login)
     app.register_blueprint(bp_perfil)
+    app.register_blueprint(bp_carrinho)
 
