7f5661a7b2fc75828e8acb9c4431afb9abf40ae5
andersonvaler <andersonvaler@gmail.com>
2021-07-18 15:39:52 -0300

    fix: solved conflicts

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -8,20 +8,14 @@ def init_app(app: Flask):
     from .carrinho_views import bp as bp_carrinho 
     from .produtos_views import bp as bp_product
     from .gerar_vendas_views import bp as bp_gerar_vendas
-<<<<<<< HEAD
     from .home_view import bp as bp_home
-=======
     from .vendas_andamento_views import bp as bp_venda_andamento
->>>>>>> 86513c8b11e1b0c4d1a621df36fbb29f104e2ede
     app.register_blueprint(bp_product)
     app.register_blueprint(bp_signup)
     app.register_blueprint(bp_login)
     app.register_blueprint(bp_perfil)
     app.register_blueprint(bp_carrinho)
     app.register_blueprint(bp_gerar_vendas)
-<<<<<<< HEAD
     app.register_blueprint(bp_home)
-=======
     app.register_blueprint(bp_venda_andamento)
->>>>>>> 86513c8b11e1b0c4d1a621df36fbb29f104e2ede
 
