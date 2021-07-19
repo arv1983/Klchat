3798bd95223af8f0074db94347e80571cef22e65
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-07-19 17:16:04 -0300

    feat endpoint compras andamento e vendas andamento

--- app/views/vendas_andamento_views.py
+++ app/views/vendas_andamento_views.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.elements import Null
 from app.models.clientes_model import Clientes
 from app.services.services import add_commit
 from app.models.lojistas_model import Lojistas
@@ -16,49 +17,42 @@ bp = Blueprint("bp_vendas_andamento", __name__)
 def venda():
     
     query_empresa = Lojistas.query.filter_by(email=get_jwt_identity()).first()
-    query_cliente =  Clientes.query.filter_by(email=get_jwt_identity()).first()
+    
+    if not query_empresa:
+        return {'erro': 'voce não é empresa'}
 
-    if query_cliente:
-        query_carrinho = Carrinho.query.filter_by(cliente_id=query_cliente.id).all()
+    data = request.get_json()
+        
+    carrinhos = []
+    produtos_lojista = Carrinho_Produto.query.filter_by(lojista_id=query_empresa.id).all()
+
+    for item in produtos_lojista:
+        if not item.carrinho_id in carrinhos:
+            carrinhos.append(item.carrinho_id)
+
+    retorna = []
+    for compra in carrinhos:
+        venda = Vendas.query.filter_by(carrinho_id=compra,status_id=data['status']).all()
+        if venda:
+            retorna.append(venda)
+            venda = ""
+
+    return jsonify(retorna)
+
+
+@bp.route("/compras-andamento", methods=["GET"])
+@jwt_required()
+def compra():
+    query_cliente = Clientes.query.filter_by(email=get_jwt_identity()).first()
+
+    if not query_cliente:
+        return {'erro': 'Essa rota é pra cliente'}
+    query_carrinho = Carrinho.query.filter_by(cliente_id=query_cliente.id).all()
         # query_vendas = Vendas.query.filter_by(carrinho_id=query_carrinho.id).first()
 
-        list_vendas = []
-        for data in query_carrinho:
-            query_vendas = Vendas.query.filter_by(carrinho_id=data.id).first()
-            if not query_vendas:
+    list_vendas = []
+    for data in query_carrinho:
+        query_vendas = Vendas.query.filter_by(carrinho_id=data.id).first()
+        if not query_vendas:
                 list_vendas.append(query_vendas)
-        return jsonify(list_vendas)
-    
-    if query_empresa:
-
-        
-        produtos_lojista = Produtos.query.filter_by(lojista_id=query_empresa.id).all()
-        print(produtos_lojista)
-        # Carrinho_Produto.query.filter_by(produto_id=)
-        # print(produtos_lojista)
-
-
-
-        # query_pivo = Carrinho_Produto.query.filter_by(cliente_id=query_cliente.id).all()
-        # query_carrinho = Vendas.query.filter_by(cliente_id=query_cliente.id).all()
-        # lojista - lojista id 1
-        # produtos - cujo lojista_id e igual ao do lojista
-        # pivo_carrinho_produtos - filtrar todos os produtos(que vao ser do lojista)
-        # vendas - filtrar todos os id carrinho da tabela pivo_carrinho_produtos
-        return {"empresa": "entrou"}
-
-
-# @bp.route("/compras-andamento", methods=["GET"])
-# @jwt_required()
-# def compra():
-#     query_cliente =  Clientes.query.filter_by(email=get_jwt_identity()).first()
-#     if query_cliente:
-#         query_carrinho = Carrinho.query.filter_by(cliente_id=query_cliente.id).all()
-#         # query_vendas = Vendas.query.filter_by(carrinho_id=query_carrinho.id).first()
-
-#         list_vendas = []
-#         for data in query_carrinho:
-#             query_vendas = Vendas.query.filter_by(carrinho_id=data.id).first()
-#             if not query_vendas:
-#                 list_vendas.append(query_vendas)
-#         return jsonify(list_vendas)
+    return jsonify(list_vendas)
