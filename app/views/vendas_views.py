92d83a068679ad0dc3204a4a597b3c2888edd33f
andersonvaler <andersonvaler@gmail.com>
2021-07-20 13:37:21 -0300

    fix: move get vendas to model vendas

--- app/views/vendas_views.py
+++ app/views/vendas_views.py
@@ -1,3 +1,4 @@
+from app.models.lojistas_model import Lojistas
 from app.services.vendas import alterar_venda, ver_venda
 from app.models.vendas_model import Vendas
 from app.exc import InputError
@@ -44,4 +45,47 @@ def ver_venda_by_id(venda_id):
 @bp.get("/vendas")
 @jwt_required()
 def ver_vendas():
-    ...
+    data = request.get_json()
+
+    print(data)
+
+    if data:
+        possible_vars = ["status"]
+        if not all(name in possible_vars for name in data.keys()):
+            keys_w = {value for value in data.keys() if not value in possible_vars}
+            return {
+                "available_keys": possible_vars,
+                "wrong_keys_sended": list(keys_w),
+            }, 422
+        if data["status"] not in range(0, 6):
+            return {"Valor do status deve ser": "1 a 5"}, 422
+
+    query_empresa = Lojistas.query.filter_by(email=get_jwt_identity()).first()
+
+    if not query_empresa:
+        return {"Error": "Vendas não disponivel para clientes."}, 401
+
+    carrinhos = []
+    produtos_lojista = Carrinho_Produto.query.filter_by(
+        lojista_id=query_empresa.id
+    ).all()
+
+    for item in produtos_lojista:
+        if not item.carrinho_id in carrinhos:
+            carrinhos.append(item.carrinho_id)
+
+    retorna = []
+    for compra in carrinhos:
+
+        if not data:
+            venda = Vendas.query.filter_by(carrinho_id=compra).all()
+        else:
+            venda = Vendas.query.filter_by(
+                carrinho_id=compra, status_id=data["status"]
+            ).all()
+
+        if venda:
+            retorna.append(venda)
+            venda = ""
+
+    return jsonify(retorna)
